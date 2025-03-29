7d43f8d8d89f43539166aa5c4111aafcbb07eac8
Leo <leonardorearte0@gmail.com>
2025-03-28 22:39:13 -0300

    Add vercel.json. Add explicit paths in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,21 @@ import { defineConfig } from "vite";
 import mdx from "@mdx-js/rollup";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
-  build: { outDir: "dist" },
+  build: {
+    outDir: "dist",
+  },
   plugins: [mdx(), react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@contexts": path.resolve(__dirname, "./src/lib/contexts"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@translations": path.resolve(__dirname, "./src/lib/translations"),
+    },
+  },
 });
